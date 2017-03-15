d01730843093940a762d2ca22a708e7491068218
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-03-15 13:22:09 +0100

    fix overlay not rendering
    The root cause remains unknown, but this fixes the problem
    
    This closes #12 and #11

--- src/DrawerLayout.js
+++ src/DrawerLayout.js
@@ -151,10 +151,12 @@ export default class DrawerLayout extends Component {
           {this.props.children}
         </Animated.View>
 
-        {drawerShown &&
-          <TouchableWithoutFeedback onPress={this._onOverlayClick}>
-            <Animated.View style={[styles.overlay, animatedOverlayStyles]} />
-          </TouchableWithoutFeedback>}
+        <TouchableWithoutFeedback onPress={this._onOverlayClick}>
+          <Animated.View
+            style={[drawerShown && styles.overlay, animatedOverlayStyles]}
+          />
+        </TouchableWithoutFeedback>
+
         <Animated.View
           style={[styles.drawer, dynamicDrawerStyles, animatedDrawerStyles]}
         >
