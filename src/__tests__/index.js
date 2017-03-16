72cfd8e4813ced018f846f66408edfe510c74d9e
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-03-16 11:42:45 +0100

    test: add empty tests

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -19,4 +19,55 @@ describe('DrawerLayout', () => {
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  describe('lifecycle methods', () => {
+    describe('componentWillMount', () => {
+      it('should add a listener to the open value');
+      it('the listener should set the drawerShown value value');
+      it('the listener should dismiss the keyboard');
+      it('the listener should call on drawer slide');
+    });
+  });
+
+  describe('pan responder', () => {
+    // TODO: also test _getOpenValueForX here
+
+    describe('panResponderRelease', () => {
+      it('should open the drawer after threshold for left');
+      it('should open the drawer according to speed for left');
+      it('should close the drawer after threshold for left');
+      it('should close the drawer according to speed for left');
+      it('should open the drawer after threshold for right');
+      it('should open the drawer according to speed for right');
+      it('should close the drawer after threshold for right');
+      it('should close the drawer according to speed for right');
+    });
+    describe('panResponderMove', () => {
+      it('should set the open value');
+      it('should set the open value to max 1');
+      it('should set the open value to min 0');
+    });
+    describe('panResponderGrant', () => {
+      it('should emit dragging');
+    });
+    describe('shouldSetPanResponder', () => {
+      it('should return false if locked');
+      // TODO: more test cases
+    });
+  });
+
+  describe('external methods', () => {
+    describe('openDrawer', () => {
+      it('should emit event');
+      it('should start animation');
+      it('should emit idle event when done with animation');
+      it('should call callback when done with animation');
+    });
+    describe('closeDrawer', () => {
+      it('should emit event');
+      it('should start animation');
+      it('should emit idle event when done with animation');
+      it('should call callback when done with animation');
+    });
+  });
 });
