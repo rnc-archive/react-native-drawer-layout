69791a1642cf9839c352074c0625979396029906
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-03-26 12:42:29 +0200

    tests(Flow): add initial flow types
    This closes #13

---
+++ flow-typed/npm/react-native_vx.x.x.js
@@ -0,0 +1,15 @@
+/**
+ * This is a libdef stub for:
+ *
+ *   'react-native'
+ *
+ * Fill this stub out by replacing all the `any` types.
+ *
+ * Once filled out, we encourage you to share your work with the
+ * community by sending a pull request to:
+ * https://github.com/flowtype/flow-typed
+ */
+
+declare module 'react-native' {
+  declare module.exports: any;
+}
